771c4a0b7971788d802ae39b8a88d4d9ff21f387
Mambo430 <junkmailonline@pm.me>
2026-01-06 14:27:17 -0500

    Update version.js

--- functions/version.js
+++ functions/version.js
@@ -1,8 +1,4 @@
 export async function onRequest(context) {
   // Update with to Date.now() to force reload.
   return Response.json({ latest: 1767549851133 });
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> upstream/main
